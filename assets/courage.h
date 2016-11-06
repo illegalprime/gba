7d4c4e08ef8e64b014ec6f8cb9b52b377ed1b64a
Michael Eden <themichaeleden@gmail.com>
2016-11-06 01:30:00 -0400

    added courage spritie

---
+++ assets/courage.h
@@ -0,0 +1,25 @@
+/*
+ * Exported with nin10kit v1.3
+ * Invocation command was nin10kit -mode=3 courage courage.jpg
+ * Time-stamp: Sunday 11/06/2016, 01:05:16
+ *
+ * Image Information
+ * -----------------
+ * courage.jpg 28@31
+ *
+ * Quote/Fortune of the Day!
+ * -------------------------
+ *
+ * All bug reports / feature requests are to be sent to Brandon (bwhitehead0308@gmail.com)
+ */
+
+#ifndef COURAGE_H
+#define COURAGE_H
+
+extern const unsigned short courage_sprite[868];
+#define COURAGE_SIZE 868
+#define COURAGE_WIDTH 28
+#define COURAGE_HEIGHT 31
+
+#endif
+
